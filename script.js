3708d4774530376e46d66218f9123b8beefc3dfb
TinhoFZ <edupassosa18@gmail.com>
2026-04-16 09:21:23 -0300

    SVGs de linguagens adicionados

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const skillContainer = document.querySelector('#skill-button-container');
+const skillContainer = document.querySelector('#skill-container');
 const currentSkill = document.querySelector('#current-skill');
 const learningSkill = document.querySelector('#learning-skill');
 
 function addSkills() {
-    const div = document.createElement('img')
-    div.setAttribute('src', 'https://devicon.dev/')
-
-    skillContainer.classList.remove('hidden')
-    skillContainer.appendChild(div)
+    skillContainer.classList.toggle('hidden');
 }
 
 currentSkill.addEventListener('click', addSkills);
